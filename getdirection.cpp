13579c228a08f564649280e3bb71f021f4607540
祺 张 <ExHalationZQ@outlook.com>
2020-10-23 10:03:46 +0800

    添加项目文件。

---
+++ getdirection.cpp
@@ -0,0 +1,31 @@
+﻿#include<iostream>
+#include <conio.h>
+using namespace std;
+int getdirection()
+{
+    int ret = 0;
+    while (ret == 0)
+    {
+        int ch = _getch();
+        if (isascii(ch))
+            continue;
+        ch = _getch();
+        switch (ch)
+        {
+        case 72:ret = 1;
+            break;
+        case 80:ret = 2;
+            break;
+        default:
+            break;
+        }
+    }
+    return ret;
+}
+int main()
+{
+    for (int k = 0; k < 10; k++)
+        cout << getdirection();
+    system("pause");
+    return 0;
+}
